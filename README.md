a22ed5cd24a85ec3e842066f8920fb2e9ca3a7ca
junting <342766475@qq.com>
2024-03-24 23:09:20 +0800

    feat: 构建链表处理执行任务&利用浏览器API（requestIdleCallback）

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ React implementation principle, a basic version.
 2. 原生 JS 方式实现
 3. 将页面元素转换成对象数据结构的方式去表达页面DOM元素的关系
 4. 将定义呈现的页面数据结构通过 render 函数转换成真实 DOM 元素呈现到 web 浏览器上。
+
+## The second day
+
+### What
+
+当渲染层级很深和很多节点时，VDOM 转换 真实 DOM，就会造成长时间卡顿，出现浏览器崩溃的情况。
+
+### How
+
+将大量的任务，拆分成多个小块，每次只执行一个小块，依次去执行。
+
+* 一个任务执行调度器，用来控制当前执行任务 => Fiber
+* 使用浏览器提供的 API： requestIdleCallback 来控制任务执行
--- core/react.js
+++ core/react.js
@@ -1,20 +1,27 @@
 function render(el, container) {
-  const dom = el.type === 'TEXT_NODE'
-    ? document.createTextNode("")
-    : document.createElement(el.type);
+  nextWorkUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    }
+  };
 
-  Object.keys(el.props)
-    .forEach((key) => {
-      if (key !== 'children') {
-        dom[key] = el.props[key];
-      }
-    })
+  // const dom = el.type === 'TEXT_NODE'
+  //   ? document.createTextNode("")
+  //   : document.createElement(el.type);
 
-  el.props.children.forEach((child) => {
-    render(child, dom);
-  })
+  // Object.keys(el.props)
+  //   .forEach((key) => {
+  //     if (key !== 'children') {
+  //       dom[key] = el.props[key];
+  //     }
+  //   })
 
-  container.append(dom);
+  // el.props.children.forEach((child) => {
+  //   render(child, dom);
+  // })
+
+  // container.append(dom);
 }
 
 function createElement(type, props, ...children) {
@@ -37,6 +44,70 @@ function createTextNode(text) {
     },
   }
 }
+
+let nextWorkUnit = null;
+function workLoop(deadline) {
+  let shouldYield = false
+  while(!shouldYield && nextWorkUnit) {
+    nextWorkUnit = preformWorkOfUnit(nextWorkUnit);
+
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function preformWorkOfUnit(work) {
+  if (!work.dom) {
+    // 1. 创建 DOM
+    const dom = (work.dom =
+      work.type === 'TEXT_NODE'
+        ? document.createTextNode("")
+        : document.createElement(work.type));
+
+    work.parent.dom.append(dom);
+
+    // 2. 处理 props
+    Object.keys(work.props)
+    .forEach((key) => {
+      if (key !== 'children') {
+        dom[key] = work.props[key];
+      }
+    });
+  }
+
+  // 3. 转换链表
+  let prevChild = null;
+  work.props.children.forEach((child, index) => {
+    const newWork = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: work,
+      sibling: null,
+      dom: null
+    };
+
+    if (index === 0) {
+      work.child = newWork;
+    } else {
+      prevChild.sibling = newWork;
+    }
+    prevChild = newWork;
+  })
+  // 4. 返回下一个执行任务
+  if (work.child) {
+    return work.child;
+  }
+
+  if (work.sibling) {
+    return work.sibling;
+  }
+
+  return work.parent?.sibling;
+}
+
 
 export default {
   render,
